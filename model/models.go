dac45bd3cc9fa365bba3036cbf50566456cd8837
Hussam Almahoozi <61137893+almahoozi@users.noreply.github.com>
2025-07-24 21:48:10 +0300

    feat(chat): add initial chat service and example tests (#1)

---
+++ model/models.go
@@ -0,0 +1,45 @@
+// Package model provides the data structures for the chat application.
+package model
+
+import (
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID          uuid.UUID
+	ImageURL    string
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+}
+
+type Chat struct {
+	ID       uuid.UUID
+	Type     ChatType
+	Admin    uuid.UUID
+	Name     string
+	ImageURL string
+	Members  []uuid.UUID
+}
+
+type ChatType string
+
+const (
+	DirectChatType ChatType = "direct"
+	GroupChatType  ChatType = "group"
+)
+
+type Message struct {
+	ID          uuid.UUID
+	SenderID    uuid.UUID
+	ChatID      uuid.UUID
+	Content     []byte
+	ContentType ContentType
+}
+
+type ContentType int
+
+const (
+	TextContentType ContentType = iota
+	ImageContentType
+)
