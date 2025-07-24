a4c9cf9082c9b2dafbc60b07ce0c2529f9586c7d
Hussam Almahoozi <ilma7oozi@gmail.com>
2025-07-24 21:26:39 +0300

    add tests and mocks to service

---
+++ service/service.go
@@ -0,0 +1,91 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"slices"
+
+	"github.com/AliUnipal/chat/model"
+	"github.com/google/uuid"
+)
+
+type ChatService interface {
+	CreateChat(ctx context.Context, participantID uuid.UUID) (uuid.UUID, error)
+	GetMessages(ctx context.Context, chatID uuid.UUID) ([]model.Message, error)
+	SendTextMessage(ctx context.Context, chatID uuid.UUID, content string) (uuid.UUID, error)
+}
+
+type ChatWithMessages struct {
+	Chat     model.Chat
+	Messages []model.Message
+}
+
+type ChatRepository interface {
+	CreateChat(ctx context.Context, chat model.Chat) (uuid.UUID, error)
+	GetChatByID(ctx context.Context, chatID uuid.UUID) (ChatWithMessages, error)
+	SendMessage(ctx context.Context, message model.Message) (uuid.UUID, error)
+}
+
+type UserContext interface {
+	GetCurrentUserID(context.Context) uuid.UUID
+}
+
+func NewService(repo ChatRepository, uc UserContext) *service {
+	return &service{repo, uc}
+}
+
+type service struct {
+	repo ChatRepository
+	uc   UserContext
+}
+
+func (s *service) GetMessages(ctx context.Context, chatID uuid.UUID) ([]model.Message, error) {
+	chatWithMessages, err := s.repo.GetChatByID(ctx, chatID)
+	if err != nil {
+		return nil, err
+	}
+
+	userID := s.uc.GetCurrentUserID(ctx)
+	if !slices.Contains(chatWithMessages.Chat.Members, userID) && chatWithMessages.Chat.Admin.String() != userID.String() {
+		return nil, fmt.Errorf("user %s is not a member of chat %s", userID.String(), chatID.String())
+	}
+
+	return chatWithMessages.Messages, nil
+}
+
+func (s *service) CreateChat(ctx context.Context, participantID uuid.UUID) (uuid.UUID, error) {
+	userID := s.uc.GetCurrentUserID(ctx)
+	chat := model.Chat{
+		ID:       uuid.New(),
+		Type:     model.DirectChatType,
+		Admin:    userID,
+		Name:     fmt.Sprintf("Chat between %s and %s", userID.String(), participantID.String()),
+		ImageURL: "",
+		Members:  []uuid.UUID{userID, participantID},
+	}
+
+	chatID, err := s.repo.CreateChat(ctx, chat)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return chatID, nil
+}
+
+func (s *service) SendTextMessage(ctx context.Context, chatID uuid.UUID, content string) (uuid.UUID, error) {
+	senderID := s.uc.GetCurrentUserID(ctx)
+	message := model.Message{
+		ID:          uuid.New(),
+		SenderID:    senderID,
+		ChatID:      chatID,
+		Content:     []byte(content),
+		ContentType: model.TextContentType,
+	}
+
+	messageID, err := s.repo.SendMessage(ctx, message)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return messageID, nil
+}
---
+++ service/service_test.go
@@ -0,0 +1,155 @@
+package service_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/AliUnipal/chat/model"
+	"github.com/AliUnipal/chat/service"
+	"github.com/AliUnipal/chat/service/mocks"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGetMessages_NonParticipantMustFail(t *testing.T) {
+	ctx := context.Background()
+	chatID := uuid.New()
+	currentUserID := uuid.New()
+	senderID := uuid.New()
+	participantID := uuid.New()
+	expectedMessages := []model.Message{
+		{ID: uuid.New(), SenderID: senderID, ChatID: chatID, Content: []byte("Hello"), ContentType: model.TextContentType},
+	}
+
+	mockUserContext := mocks.NewUserContext(t)
+	mockUserContext.EXPECT().GetCurrentUserID(mock.Anything).Return(currentUserID)
+
+	mockRepo := mocks.NewChatRepository(t)
+	mockRepo.EXPECT().GetChatByID(mock.Anything, chatID).Return(service.ChatWithMessages{
+		Chat: model.Chat{
+			ID:       chatID,
+			Type:     model.DirectChatType,
+			Admin:    uuid.New(),
+			Name:     "Something",
+			ImageURL: "",
+			Members:  []uuid.UUID{senderID, participantID},
+		},
+		Messages: expectedMessages,
+	}, nil)
+
+	service := service.NewService(mockRepo, mockUserContext)
+
+	messages, err := service.GetMessages(ctx, chatID)
+	if err == nil {
+		t.Log("expected error, got nil")
+		t.Fail()
+	}
+
+	if len(messages) != 0 {
+		t.Fatalf("expected 0 messages, got %d", len(messages))
+	}
+}
+
+func TestGetMessages_AdminGetsMessages(t *testing.T) {
+	ctx := context.Background()
+	chatID := uuid.New()
+	currentUserID := uuid.New()
+	senderID := uuid.New()
+	participantID := uuid.New()
+	expectedMessages := []model.Message{
+		{ID: uuid.New(), SenderID: senderID, ChatID: chatID, Content: []byte("Hello"), ContentType: model.TextContentType},
+	}
+
+	mockUserContext := mocks.NewUserContext(t)
+	mockUserContext.EXPECT().GetCurrentUserID(mock.Anything).Return(currentUserID)
+
+	mockRepo := mocks.NewChatRepository(t)
+	mockRepo.EXPECT().GetChatByID(mock.Anything, chatID).Return(service.ChatWithMessages{
+		Chat: model.Chat{
+			ID:       chatID,
+			Type:     model.DirectChatType,
+			Admin:    currentUserID,
+			Name:     "Something",
+			ImageURL: "",
+			Members:  []uuid.UUID{senderID, participantID},
+		},
+		Messages: expectedMessages,
+	}, nil)
+
+	service := service.NewService(mockRepo, mockUserContext)
+
+	messages, err := service.GetMessages(ctx, chatID)
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	if len(messages) != len(expectedMessages) {
+		t.Fatalf("expected %d messages, got %d", len(expectedMessages), len(messages))
+	}
+
+	for i, msg := range messages {
+		if msg.ID != expectedMessages[i].ID || msg.SenderID != expectedMessages[i].SenderID || msg.ChatID != expectedMessages[i].ChatID || string(msg.Content) != string(expectedMessages[i].Content) || msg.ContentType != expectedMessages[i].ContentType {
+			t.Errorf("expected message %v, got %v", expectedMessages[i], msg)
+		}
+	}
+}
+
+func TestGetMessages_GetsMessages(t *testing.T) {
+	ctx := context.Background()
+	chatID := uuid.New()
+	currentUserID := uuid.New()
+	participantID := uuid.New()
+	expectedMessages := []model.Message{
+		{ID: uuid.New(), SenderID: currentUserID, ChatID: chatID, Content: []byte("Hello"), ContentType: model.TextContentType},
+		{ID: uuid.New(), SenderID: participantID, ChatID: chatID, Content: []byte("Hi"), ContentType: model.TextContentType},
+	}
+
+	mockUserContext := mocks.NewUserContext(t)
+	mockUserContext.EXPECT().GetCurrentUserID(mock.Anything).Return(currentUserID)
+
+	mockRepo := mocks.NewChatRepository(t)
+	mockRepo.EXPECT().GetChatByID(mock.Anything, chatID).Return(service.ChatWithMessages{
+		Chat: model.Chat{
+			ID:       chatID,
+			Type:     model.DirectChatType,
+			Admin:    currentUserID,
+			Name:     "Something",
+			ImageURL: "",
+			Members:  []uuid.UUID{currentUserID, participantID},
+		},
+		Messages: expectedMessages,
+	}, nil)
+
+	service := service.NewService(mockRepo, mockUserContext)
+
+	messages, err := service.GetMessages(ctx, chatID)
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	if len(messages) != len(expectedMessages) {
+		t.Fatalf("expected %d messages, got %d", len(expectedMessages), len(messages))
+	}
+
+	for i, msg := range messages {
+		if msg.ID != expectedMessages[i].ID || msg.SenderID != expectedMessages[i].SenderID || msg.ChatID != expectedMessages[i].ChatID || string(msg.Content) != string(expectedMessages[i].Content) || msg.ContentType != expectedMessages[i].ContentType {
+			t.Errorf("expected message %v, got %v", expectedMessages[i], msg)
+		}
+	}
+}
+
+func TestGetMessages_ReturnsErrors(t *testing.T) {
+	ctx := context.Background()
+	chatID := uuid.New()
+
+	mockRepo := mocks.NewChatRepository(t)
+	mockRepo.EXPECT().GetChatByID(mock.Anything, chatID).Return(service.ChatWithMessages{}, errors.New("chat not found"))
+
+	mockUserContext := mocks.NewUserContext(t)
+	service := service.NewService(mockRepo, mockUserContext)
+
+	if _, err := service.GetMessages(ctx, chatID); err == nil {
+		t.Fatal("expected error, got nil")
+	}
+}
